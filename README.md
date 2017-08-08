4d0108d7f630d7696aba2493993985393864a798
bostin.wang <wangbo78978@126.com>
2017-08-08 17:21:04 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# XLSXWriter
+Fork from [mk-j/PHP_XLSXWriter](https://github.com/mk-j/PHP_XLSXWriter)
+
+移入到命名空间：
+```
+namespace Bostin\Office\Excel;
+```
+
+
+添加了一个方法：
+```
+use Bostin\Office\Excel\XLSXWriter;
+
+$data = array(
+    array('year','month','amount'),
+    array('2003','1','220'),
+    array('2003','2','153.5'),
+);
+
+$writer = new XLSXWriter();
+$writer->writeSheet($data);
+$writer->outputToBrowser('output.xlsx'); // 即通过浏览器下载xlsx文件
+```
+
+
+
---
+++ examples/ex05-numbers-250k-rows.php
@@ -0,0 +1,14 @@
+<?php
+include_once '../src/XLSXWriter/BuffererWriter.php';
+include_once '../src/XLSXWriter.php';
+
+use Bostin\Office\Excel\XLSXWriter;
+
+$writer = new XLSXWriter();
+$writer->writeSheetHeader('Sheet1', array('c1'=>'integer','c2'=>'integer','c3'=>'integer','c4'=>'integer') );//optional
+for($i=0; $i<250000; $i++)
+{
+    $writer->writeSheetRow('Sheet1', array(rand()%10000,rand()%10000,rand()%10000,rand()%10000) );
+}
+$writer->writeToFile('xlsx-numbers-250k.xlsx');
+echo '#'.floor((memory_get_peak_usage())/1024/1024)."MB"."\n";
